2eb7908d856290c172104f91f2d24bb1abbe7fab
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-03-14 11:43:17 +0100

    installation mongo db

--- P7-Dev-Web-livres-main/Backend/app.js
+++ P7-Dev-Web-livres-main/Backend/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Book = require('./models/Book');
+
+mongoose.connect('mongodb+srv://oce-bert-31:M1N2O1303@cluster1.fvu9kvo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 const app = express();
-
-app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -11,33 +18,68 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/stuff', (req, res) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
-    });
+app.use(express.json());
+
+app.post('/api/books', (req, res, next) => {
+  delete req.body._id;
+  const book = new Book({
+    ...req.body
+  });
+  book.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
-app.get('/api/stuff', (req, res) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+app.put('/api/books/:id', (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id', (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// app.get('/api/stuff', (req, res) => {
+//     const stuff = [
+//       {
+//         _id: 'oeihfzeoi',
+//         title: 'Mon premier objet',
+//         description: 'Les infos de mon premier objet',
+//         imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+//         price: 4900,
+//         userId: 'qsomihvqios',
+//       },
+//       {
+//         _id: 'oeihfzeomoihi',
+//         title: 'Mon deuxième objet',
+//         description: 'Les infos de mon deuxième objet',
+//         imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+//         price: 2900,
+//         userId: 'qsomihvqios',
+//       },
+//     ];
+//     res.status(200).json(stuff);
+// });
+
+app.get('/api/books/:id', (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/books', (req, res, next) => {
+  Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
+
+
+
+// mongodb+srv://oce-bert-31:<password>@cluster1.fvu9kvo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1
+
+// mongodb+srv://oce-bert-31:<password>@cluster1.fvu9kvo.mongodb.net/
